65b202cb0624df6a1a2a07477358cb6f9f0a8f3c
CrisMoreiras <137789637+CrisMoreiras@users.noreply.github.com>
2024-01-11 21:07:54 +0000

    04

--- src/js/views/swhome.js
+++ src/js/views/swhome.js
@@ -1,13 +1,23 @@
-import React from "react";
+import React, {useEffect} from "react";
 import "../../styles/home.css";
 
 import { Character } from "../component/character";
 
-export const Swhome = () => (
-	<div className="text-center mt-5">
-		<h1>HOME BLOG SW</h1>
+export const Swhome = () => {
 
-        < Character />
+    useEffect ( () =>{
+        console.log ("cargo home")
+        fetch ("https://www.swapi.tech/api/planets")
+        .then ((response)=>response.json())
+        .then ((data)=>console.log(data.results))
+    } ,[])
+
+    return (
+
+	<div className="text-center mt-5">
+		< Character name= "props 1" />
+        < Character name= "props 2" />
 	</div>
     
 );
+}
