94738f907eac0ea33bdda2255820cc74cecc354d
CrisMoreiras <137789637+CrisMoreiras@users.noreply.github.com>
2024-01-11 23:07:42 +0000

    15

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -9,21 +9,9 @@ export const Home = () => {
 
 	const { store, actions } = useContext(Context);
 
-    const [people, setPeople] = useState ([])
-
-    useEffect ( () =>{
-        console.log ("cargo home")
-        fetch ("https://www.swapi.tech/api/people")
-        .then ((response)=>response.json())
-        .then ((data)=>setPeople(data.results))
-    } ,[])
-
     return (
 
 	<div className="text-center mt-5">
-
-		<h1>Desde Componente</h1>
-        {people.map((item) => < Character key={item.name} name= {item.name} /> )}
 
 		<h1>Desde Flux</h1>
         {store.characters.map((item) => < Character key={item.name} name= {item.name} /> )}
