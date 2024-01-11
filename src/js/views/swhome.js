ebddaceadc046a70ff9f75981b1ee8983b04a96a
CrisMoreiras <137789637+CrisMoreiras@users.noreply.github.com>
2024-01-11 21:24:19 +0000

    05

--- src/js/views/swhome.js
+++ src/js/views/swhome.js
@@ -1,20 +1,25 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import "../../styles/home.css";
 
 import { Character } from "../component/character";
 
 export const Swhome = () => {
 
+    const [people, setPeople] = useState ([])
+
     useEffect ( () =>{
         console.log ("cargo home")
-        fetch ("https://www.swapi.tech/api/planets")
+        fetch ("https://www.swapi.tech/api/people")
         .then ((response)=>response.json())
-        .then ((data)=>console.log(data.results))
+        .then ((data)=>setPeople(data.results))
     } ,[])
 
     return (
 
 	<div className="text-center mt-5">
+
+        {people.map((item) => <p> {item.name} </p> )}
+
 		< Character name= "props 1" />
         < Character name= "props 2" />
 	</div>
